ebeca8a08124d8a31c636c71f7846c32301f84e5
Luan Rios Campos <luan.rios.campos@gmail.com>
2017-03-16 23:28:20 -0300

    search and play a playlist based on emotion

--- app.js
+++ app.js
@@ -7,7 +7,22 @@
 // token = spotifyApi.getAccessToken();
 // spotifyApi.setAccessToken(token);
 
+const SpotifyWebApi = require('spotify-web-api-node');
+const SpotifyControl = require('spotify-control');
+
+var spotifyApi = new SpotifyWebApi({
+  clientId: '5bbdc263f42244bdb1da7ccfd8d33de6',
+  clientSecret: '922ba87fb9f64183a27016b1721220c1',
+  accessCode: 'NAowChgKB1Nwb3RpZnkSABoGmAEByAEBJWKvy1gSFHNPKPoprAvIKc-jtdcHFsvppHwb'
+
+});
+
+var spotifyControl = new SpotifyControl({
+    token: "NAowChgKB1Nwb3RpZnkSABoGmAEByAEBJY-wy1gSFCSkr4L80W3-YpgwNf-UPfyZlJp3"
+});
+
 var demographicsPromise = matrix.service('demographics').start();
+
 demographicsPromise.then(function(demographicsData) {
   var emotion = demographicsData.emotion;
   if (emotion === "HAPPY"){
@@ -24,4 +39,35 @@ demographicsPromise.then(function(demographicsData) {
     matrix.led('blue');
   }
 
+
+  spotifyApi.searchPlaylists(emotion, {
+    country: 'BR',
+    limit: 10
+  }, function (err, data) {
+
+    if(err) return console.log(err);
+
+    var playlists = data.body.playlists.items;
+    var playlist_index = Math.floor((Math.random() * 10));
+
+    play(playlists[playlist_index].uri);
+  });
+
 });
+
+function play(uri) {
+
+  spotifyControl.connect().then(v => {
+      console.log("Started");
+      spotifyControl.play(uri).then(v => {
+          console.log("playing");
+          spotifyControl.startListener(["play", "pause"]).on("event", data => {
+              console.log(JSON.stringify(data, null, 4));
+          });
+      }, err => {
+          console.error(err);
+      });
+  }, err => {
+      console.error("Failed to start: " + err.message);
+  });
+}
