c932ff87750a83197741eb8958d0de89c223d148
Lucas <lucas.ecomp2012@gmail.com>
2017-04-18 21:49:15 -0300

    Diego & EU

--- app.js
+++ app.js
@@ -1,69 +1,61 @@
 var SpotifyWebApi = require('spotify-web-api-node');
 var nodemailer = require('nodemailer');
-var async = require('async');
-var spotifyApi = new SpotifyWebApi({
-  clientId: '5bbdc263f42244bdb1da7ccfd8d33de6',
-  clientSecret: '922ba87fb9f64183a27016b1721220c1',
-  accessCode: 'NAowChgKB1Nwb3RpZnkSABoGmAEByAEBJWKvy1gSFHNPKPoprAvIKc-jtdcHFsvppHwb'
-});
-var totalLink  = '';
 
 var algorithm = 'demographics';
+var service = matrix.service(algorithm);
+
+var spotifyApi = new SpotifyWebApi({
+    clientId: '5bbdc263f42244bdb1da7ccfd8d33de6',
+    clientSecret: '922ba87fb9f64183a27016b1721220c1',
+    accessCode: 'NAowChgKB1Nwb3RpZnkSABoGmAEByAEBJWKvy1gSFHNPKPoprAvIKc-jtdcHFsvppHwb'
+});
+
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'admolizetest@gmail.com',
-     pass: 'tapioca123'
+        user: 'admolizetest@gmail.com',
+        pass: 'tapioca123'
     }
 });
-function detect(){
-    async.waterfall([
-        function(callback){
-            matrix.service(algorithm).start().then(function(data){
-                var emotions = {"HAPPY": "yellow", "SAD": "red", "CONFUSED": "blue", "ANGRY": "green", "CALM": "white", "SURPRISED": "purple", "DISGUST": "brown"};
-                if (Object.keys(data.demographics).length > 0){
-                    matrix.led(emotions[data.demographics.emotion]).render();
-                    matrix.service('demographics').stop();
-                    return callback(null, data.demographics.emotion);
-                }
-                return callback("No demographics");
-            });
-        }, function(emotion, callback) {
-            matrix.service(algorithm).stop();
-            spotifyApi.searchPlaylists(emotion)
-                .then(function(data){
-                    var playlists = data.body.playlists.items;
-                    var playlists_index = Math.floor((Math.random() * 10));
-                    var res = playlists[playlists_index].uri;
-                    res = res.replace(':', '/');
-                    res = res.split("spotify");
 
-                    totalLink = "play.spotify.com"+res[1];
+service.start().then(function(data) {
+    service.stop();
+    var emotions = {"HAPPY": "yellow", "SAD": "red", "CONFUSED": "blue", "ANGRY": "green", "CALM": "white", "SURPRISED": "purple", "DISGUST": "brown"};
 
-                    return callback(null, { link: totalLink, emotion: emotion });
-                }, function(error) {
-                    return callback(error);
-                });
-        }, function(data, callback) {
+    if (Object.keys(data.demographics).length > 0) {
+        matrix.led(emotions[data.demographics.emotion]).render();
+        var emotion = data.demographics.emotion;
+
+        spotifyApi.searchPlaylists(emotion)
+        .then(function(response) {
+            var playlists = response.body.playlists.items;
+            var playlists_index = Math.floor((Math.random() * 10));
+            var res = playlists[playlists_index].uri;
+            res = res.replace(':', '/');
+            res = res.split("spotify");
+
+            var data = { link: "play.spotify.com"+res[1], emotion: emotion };
             var mailOptions = {
-                from: '"Fred Foo 👻" <pedro@moodify.com>', // sender address
+                from: '"Porque undefined, @Deus?  👻" <pedro@moodify.com>', // sender address
                 to: 'lucas.morais@admobilize.com, pedroecomp@gmail.com', // list of receivers
                 subject: 'Hello ✔', // Subject line
-                text: 'Seu humor é '+ data.emotion, // plain text body
-                html: "<a> " + data.totalLink +"</a>" // html body
+                html: "Seu humor é " + data.emotion + " <a> " + data.link + "</a>" // html body
             };
+
             // send mail with defined transport object
             transporter.sendMail(mailOptions, function (error, info)  {
-                if (error) return callback(error);
-                console.log('Message %s sent: %s', info.messageId, info.response);
-                return callback(null, data.totalLink);
-            });
-        }
-        ], function(error, data){
-            if (error) return callback(error);
-            matrix.send({ playlist: data.totalLink });
-        });
-}
+                if(error) {
+                    console.log('\n\n 1 ===================================================>');
+                    console.log(error);
+                    console.log('===================================================>\n\n');
+                }
 
-detect();
-matrix.send({playlist: totalLink});
+                matrix.send({ playlist: data.link });
+            });
+        }, function(error) {
+            console.log('\n\n 2 ===================================================>');
+            console.log(error);
+            console.log('===================================================>\n\n');
+        });
+    }
+});
